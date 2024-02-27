f4ea322bf60fd4fdf069e2f725c723f0e83ec179
somebody3232 <samuelshuster@gmail.com>
2024-02-27 11:04:37 -0500

    Fix Tailwind and Start Home/Flag Entry Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
     // TODO: Flag input menu (& maybe server validation [must be a valid flag])
 </script>
-<div class="w-full bg-gray-800 min-h-screen">
-    <p class="text-white">WriteDown</p>
+<div class="w-full bg-gray-800 min-h-screen flex flex-col items-center justify-center px-5">
+    <div class="pb-10 flex flex-col items-center text-white">
+        <h1 class="text-white text-4xl font-black">WriteDown</h1>
+        <h4 class="text-lg font-bold">The CTF Writeup Sharing Solution</h4>
+    </div>
+    <div class="flex flex-col items-center justify-around border-gray-500 bg-gray-700 border rounded-2xl h-[80vh] w-full md:w-7/12">
+        <input class="bg-gray-900 w-[20rem] border-gray-500 border-2 rounded text-white" type="text" placeholder="prefix{`{__FLAG__}`}">
+    </div>
 </div>
